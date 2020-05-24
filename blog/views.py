ab50b871ac18ca0e1014e03d6a78f26c439f52d0
MrEscape54 <martino.guzman@gmail.com>
2020-05-23 22:27:51 -0300

    paginator

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView, ListView, DetailView
+
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
 from .models import Post, User
 
-class IndexView(TemplateView):
-    template_name = "blog/index.html"
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 4
+    template_name = "blog/list.html"
 
-def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'post/list.html', {'posts': posts})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='publicado', publish__year=year, publish__month=month, publish__day=day)
